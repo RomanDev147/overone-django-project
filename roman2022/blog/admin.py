377b7409d033aff26a8caf0293075162b3e043f6
Roman Parkachev <romanparkachev@gmail.com>
2022-07-04 14:37:23 +0300

    Django project created

---
+++ roman2022/blog/admin.py
@@ -0,0 +1,10 @@
+# login: romanparkachev
+# password: 777888999
+
+from django.contrib import admin
+from .models import Post, Category
+
+admin.site.register(Post)
+admin.site.register(Category)
+
+
---
+++ roman2022/project/admin.py
@@ -0,0 +1,9 @@
+# login: romanparkachev
+# password: 777888999
+
+from django.contrib import admin
+from .models import Project
+
+admin.site.register(Project)
+
+
