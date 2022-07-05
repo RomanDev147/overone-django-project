cd1c3b9dcd501968cb21c69c884fdd9f2b24f7cd
Roman Parkachev <romandev147@gmail.com>
2022-07-06 00:04:05 +0300

    Hiding key

--- roman2022/roman2022/settings.py
+++ roman2022/roman2022/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-rzfcvgy&koeqgl7ss6$yis1h2b$q95rq!xoqzgu8qx!-c&(2&t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
